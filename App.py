ff8b572f0a547b49c3f9ae287bd6ec05ba397dd1
Shweta Shaw <shweta.shaw@coviam.com>
2017-07-09 20:44:33 +0530

    Adding version extraction WIP

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ import json
 from threading import Thread
 import threading
 import jenkinProperty
+import time
 
 
 class AppThread(Thread):
@@ -12,9 +13,88 @@ class AppThread(Thread):
         self.threadNo = threadNo
         self.listOfApp = listOfApp
 
+    def getVersion(self, app):
+    	url = "http://lrathod:lrathod@localhost:8080/job/"+app.appName+"/lastStableBuild/api/json"
+        response = requests.get(url);
+        content = json.loads(response.content)
+        parameters = content['actions'][0]['parameters']
+        for parameter in parameters:
+            paramName = parameter['name']
+            if (paramName == 'version'):
+                lastSuccessBuildVersion = parameter['value']
+                print 'version is ', lastSuccessBuildVersion
+                break
+
+        print 'final version is ', lastSuccessBuildVersion
+
+        buildNumberPostfix = "-1";
+        versionToUse = app.initialVersion + buildNumberPostfix;
+
+        appVersion = app.initialVersion;
+        if lastSuccessBuildVersion and lastSuccessBuildVersion.startswith(appVersion):
+            lastIndex = lastSuccessBuildVersion.rfind("-");
+            buildNumber = lastSuccessBuildVersion[lastIndex+1:]
+            buildNumber = int(buildNumber) +1;
+            buildNumberPostfix = '-' + str(buildNumber)
+            print 'buildNumber ', buildNumber
+
+
+        versionToUse = appVersion + buildNumberPostfix
+
+        print versionToUse
+        return versionToUse
+
+    def constructBuildAPIURL(self, appname, branch, version):
+    	url = jenkinProperty.jenkinsSchema + jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort;
+    	url = url + "/job/"+app.appName+"/buildWithParameters?token="+jenkinProperty.apiToken+"&version="+app.initialVersion+"&branch="+app.branchName
+    	return url
+
     def run(self):
     	print self.threadNo
-    	print jenkinProperty.jenkinCrumb
+
+    	for app in self.listOfApp:
+    		print self.threadNo
+    		print app.branchName
+	    	headers={"Jenkins-Crumb": jenkinProperty.jenkinCrumb}
+	    	print "Start Building"
+	    	version = self.getVersion(app)
+
+	        url = self.constructBuildAPIURL(app.appName, version , app.branchName)
+	        # "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/buildWithParameters?token=KK044zgm0POwZK5zm4IMf2upUbIDnfy8&version="+app.initialVersion+"&branch="+app.branchName
+	        print 'api url is ', url 
+	        requests.post(url, headers=headers)
+	        time.sleep(jenkinProperty.pollingTime)
+	        print "Start Polling"
+	        url = "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/lastBuild/api/json"
+	        response = requests.get(url, headers=headers)
+	        content = json.loads(response.content)
+	        # print "lastBuild response ", content
+	        building = content['building']
+	        result = content['result']
+	        print "build ", building
+	        print "result ", result
+
+	        while building == True:
+	            time.sleep(jenkinProperty.pollingTime)
+	            url = "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/lastBuild/api/json"
+	            response = requests.get(url)
+	            content = json.loads(response.content)
+	            building = content['building']
+	            result = content['result']
+
+	            print "build ", building
+	            print "result ", result
+
+
+	        # print "final result ", result
+            #while 
+            #building = res[buildLength + 10 : buildLength + 11]
+            #result = res[resultLength + 9 : resultLength + 10]
+            # if building == "t":
+            # 	threading.Timer(30.0, checkThreadState(building, result, self.url, headers)).start()
+            # 	buildingFinished = checkThreadState(building, result,self.appName)
+            # 	print "Building Finish :" + buildingFinished
+
 
 
 class App(object):
--- jenkinProperty.py
+++ jenkinProperty.py
@@ -1,3 +1,8 @@
+apiToken="KK044zgm0POwZK5zm4IMf2upUbIDnfy8"
+jenkinsSchema="http://"
+jenkinsHost="localhost"
+jenkinsPort="8080"
+pollingTime=20
 jenkinCrumb="482a316e3c5d692a6de169bfd79a0a39"
 userName="likhesh"
 password="likhesh"
