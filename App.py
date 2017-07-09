80eed059e0fd7d42332243ecdee391557ad2b4d1
Shweta Shaw <shweta.shaw@coviam.com>
2017-07-10 01:27:47 +0530

    Add build API

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ class AppThread(Thread):
         self.listOfApp = listOfApp
 
     def getVersion(self, app):
-    	url = self.constructPollAPIURL(app.appName)
+        url = self.constructPollAPIURL(app.appName)
         print 'Poll API URL ', url
         response = requests.get(url);
         buildNumberPostfix = "-1";
@@ -42,13 +42,13 @@ class AppThread(Thread):
                     if lastSuccessBuildVersion and lastSuccessBuildVersion.startswith(appVersion):
                         lastIndex = lastSuccessBuildVersion.rfind("-");
                         if lastIndex > 1:
-            	            buildNumber = lastSuccessBuildVersion[lastIndex+1:]
-            	            #print "LAST", lastSuccessBuildVersion
-            	            #print "BUILD", buildNumber
-            	            buildNumber = int(buildNumber) + 1;
-            	            buildNumberPostfix = '-' + str(buildNumber)
-            	            #print 'buildNumber ', buildNumber
-            	            versionToUse = appVersion + buildNumberPostfix
+                            buildNumber = lastSuccessBuildVersion[lastIndex+1:]
+                            #print "LAST", lastSuccessBuildVersion
+                            #print "BUILD", buildNumber
+                            buildNumber = int(buildNumber) + 1;
+                            buildNumberPostfix = '-' + str(buildNumber)
+                            #print 'buildNumber ', buildNumber
+                            versionToUse = appVersion + buildNumberPostfix
                     break
         print 'version to use is for ', app.appName ,'is ', versionToUse
         return versionToUse
@@ -59,54 +59,80 @@ class AppThread(Thread):
         return url
 
     def constructBuildAPIURL(self, appname, branch, version):
-    	url = jenkinProperty.jenkinsSchema + jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort;
-    	url = url + "/job/"+appname+"/buildWithParameters?token="+jenkinProperty.apiToken+"&version="+version+"&branch="+branch
-    	return url
+        url = jenkinProperty.jenkinsSchema + jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort;
+        url = url + "/job/"+appname+"/buildWithParameters?token="+jenkinProperty.apiToken+"&version="+version+"&branch="+branch
+        return url
+
+    def constructDeployAPIURL(self, appname, version):
+        url = jenkinProperty.jenkinsSchema + jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort;
+        url = url + "/job/"+appname+"/buildWithParameters?token="+jenkinProperty.apiToken+"&version="+version
+        return url
+
+    def getDeployJobName(self, buildJobName):
+        return buildJobName+"-deploy"
 
     def run(self):
-    	print self.threadNo
+        #print self.threadNo
+        headers={"Jenkins-Crumb": jenkinProperty.jenkinCrumb}
+        appsToDeploy = []
+        for app in self.listOfApp:
+            # print self.threadNo
+            # print app.branchName
+            # print "Start Building"
+            version = self.getVersion(app)
 
-    	for app in self.listOfApp:
-    		print self.threadNo
-    		print app.branchName
-	    	headers={"Jenkins-Crumb": jenkinProperty.jenkinCrumb}
-	    	print "Start Building"
-	    	version = self.getVersion(app)
+            url = self.constructBuildAPIURL(app.appName, app.branchName, version)
+            # print 'api url is ', url 
+            requests.post(url, headers=headers)
+            time.sleep(jenkinProperty.pollingTime)
+            # print "Start Polling"
+            url = "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/lastBuild/api/json"
+            response = requests.get(url, headers=headers)
+            content = json.loads(response.content)
+            # print "lastBuild response ", content
+            building = content['building']
+            result = content['result']
+            # print "build ", building
+            # print "result ", result
+            global result1
+            while building == True:
+                global result1
+                time.sleep(jenkinProperty.pollingTime)
+                url = "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/lastBuild/api/json"
+                response = requests.get(url)
+                content = json.loads(response.content)
+                building = content['building']
+                result1 = content['result']
 
-	        url = self.constructBuildAPIURL(app.appName, app.branchName, version)
-	        print 'api url is ', url 
-	        requests.post(url, headers=headers)
-	        time.sleep(jenkinProperty.pollingTime)
-	        print "Start Polling"
-	        url = "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/lastBuild/api/json"
-	        response = requests.get(url, headers=headers)
-	        content = json.loads(response.content)
-	        # print "lastBuild response ", content
-	        building = content['building']
-	        result = content['result']
-	        print "build ", building
-	        print "result ", result
+                # print "build ", building
+                # print "result ", result1
+            print "RESULT "+result1
+            appDetails = []
+            if result1 == 'SUCCESS':
+                appDetails.append(version)
+                appDetails.append(app.appName)
+                appsToDeploy.append(appDetails)
 
-	        while building == True:
-	            time.sleep(jenkinProperty.pollingTime)
-	            url = "http://"+jenkinProperty.userName+":"+jenkinProperty.password+"@"+jenkinProperty.jenkinsHost+":"+jenkinProperty.jenkinsPort+"/job/"+app.appName+"/lastBuild/api/json"
-	            response = requests.get(url)
-	            content = json.loads(response.content)
-	            building = content['building']
-	            result = content['result']
+        #Deploying
+        i = 0
+        for apps in appsToDeploy:
+            print "Deploying"
+            print self.getDeployJobName(apps[1])
+            url = self.constructDeployAPIURL(self.getDeployJobName(apps[1]),apps[0])
+            print url
+            requests.post(url, headers=headers)
+            i += 1
 
-	            print "build ", building
-	            print "result ", result
 
 class App(object):
 
-	def __init__(self, appName, branchName, initialVersion):
-		self.appName = appName
-		self.branchName = branchName
-		self.initialVersion = initialVersion
+    def __init__(self, appName, branchName, initialVersion):
+        self.appName = appName
+        self.branchName = branchName
+        self.initialVersion = initialVersion
 
-	def displayDetails(self):
-		print "AppName : ", self.appName,  ", BranchName: ", self.branchName,  ", Version: ", self.initialVersion
+    def displayDetails(self):
+        print "AppName : ", self.appName,  ", BranchName: ", self.branchName,  ", Version: ", self.initialVersion
 
 
 with open('dep.json') as data_file:    
@@ -115,15 +141,15 @@ with open('dep.json') as data_file:
 parallel = []
 
 for majorkeys, subdict in data.iteritems():
-	sequential = []
-	for subkeys, value in subdict.iteritems():
-		test = App(value["appName"], value["branchName"], value["initialVersion"])
-		sequential.append(test)
-	parallel.append(sequential)
+    sequential = []
+    for subkeys, value in subdict.iteritems():
+        test = App(value["appName"], value["branchName"], value["initialVersion"])
+        sequential.append(test)
+    parallel.append(sequential)
 
 temp = 0
 for i in parallel:
-	t = AppThread(temp,i)
-	t.start()
-	temp += 1
+    t = AppThread(temp,i)
+    t.start()
+    temp += 1
 
